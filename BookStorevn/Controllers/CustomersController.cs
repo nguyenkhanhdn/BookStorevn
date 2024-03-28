e468692076e95ddcb3bc3023c3283f5615f3eb5e
ĐN - Nguyễn Khánh <khanhdsp@gmail.com>
2024-03-28 20:56:35 +0700

    ddd

--- BookStorevn/Controllers/CustomersController.cs
+++ BookStorevn/Controllers/CustomersController.cs
@@ -54,14 +54,14 @@ namespace BookStorevn.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FullName,Address,Email,Phone")] Customer customer)
+        public async Task<IActionResult> Create([Bind("Id,FullName,Address,Email,Phone")] Customer customer)
         {
-            //if (ModelState.IsValid)
-            //{
+            if (ModelState.IsValid)
+            {
                 _context.Add(customer);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
-            //}
+            }
             return View(customer);
         }
 
